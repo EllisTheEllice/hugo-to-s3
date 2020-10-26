101ec6578cd21ad2fdc7b2faf32331abd4386875
ENGBESI <simon.engbers@capgemini.com>
2020-10-26 06:55:12 +0100

    fixed some minor bugs

--- docker/start.sh
+++ docker/start.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 OUTPATH=./s3out.txt
 FILESPATH=./files.txt
+OUTPUTFOLDER=public
 
 
 hugo -D --minify
-aws s3 sync public/ s3://$S3_BUCKET/ --size-only --exclude ".*" --dryrun > $OUTPATH
-aws s3 sync public/ s3://$S3_BUCKET/ --size-only --exclude ".*"
+cd $OUTPUTFOLDER
+aws s3 sync . s3://$S3_BUCKET/ --size-only --exclude ".*" --dryrun > $OUTPATH
+aws s3 sync . s3://$S3_BUCKET/ --size-only --exclude ".*"
 
+#test if the outputfile exists. Only if it does, proceed
 if test -f "$OUTPATH"; then
-
-  echo "Now invalidate..."
-  cat $OUTPATH
 
   while IFS= read -r line
   do
     echo -n "$line" | sed "s/^.*upload: \(.*\) to.*/\"\1\" /" >> $FILESPATH
   done < "$OUTPATH"
 
+  #test if filespath exists and contains the list of files to invalidate. Only if it exists, proceed
   if test -f "$FILESPATH"; then
     echo "Send invalidation request..."
     echo "aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "$(cat $FILESPATH)
-    aws cloudfront create-invalidation --distribution-id $ID --paths $(cat $FILESPATH)
+    aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths $(cat $FILESPATH)
   fi
 
 fi
